a1d8a3d9a84299d73db49dbda55cdfbab0951ec5
Jonathan Bispo <jokz.bis@gmail.com>
2026-04-05 02:57:21 -0300

    learn: encerra codigo 2.5.4
    - vimos o grid system do bootstrap
    - a solucao esta adaptada para lidar com quatro pedidos
    - o mecanismo de responsividade foi atualizado

---
+++ ap04-props/src/App.jsx
@@ -0,0 +1,27 @@
+const App = () => (
+  <div className="conteiner border mt-2">
+    <div className="row">
+      <div className="col-12">
+        {/* i.fa.fa-hippo */}
+        <i className="fa fa-hippo fa-2x p-3"></i>
+      </div>
+    </div>
+
+    <div className="row">
+      <div className="col-sm-12 col-md-6 col-xxl-3">
+        <div className="py-2 border">Pedido 1</div>
+      </div>
+      <div className="col-sm-12 col-md-6 col-xxl-3">
+        <div className="py-2 border">Pedido 2</div>
+      </div>
+      <div className="col-sm-12 col-md-6 col-xxl-3">
+        <div className="py-2 border">Pedido 3</div>
+      </div>
+      <div className="col-sm-12 col-md-6 col-xxl-3">
+        <div className="py-2 border">Pedido 4</div>
+      </div>
+    </div>
+  </div>
+)
+
+export default App
