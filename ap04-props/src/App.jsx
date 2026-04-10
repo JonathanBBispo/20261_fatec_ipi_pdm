f466e9ea04d5f9f5e4190507723523345f7ea726
Jonathan Bispo <jokz.bis@gmail.com>
2026-04-09 23:03:19 -0300

    learn(ap04): encerra a apostila
    - no final, foi proposto o exercicio da apostila 04

--- ap04-props/src/App.jsx
+++ ap04-props/src/App.jsx
@@ -1,5 +1,20 @@
-const App = () => (
-  <div className="conteiner border mt-2">
+import Pedido from "./Pedido"
+import Cartao from "./Cartao"
+import Feedback from "./Feedback"
+const App = () => {
+  const textoOK = "Já recebi"
+  const textoNOK = "Ainda não recebi"
+  const funcaoOK = () => alert('Agradecemos o feedback')
+  const funcaoNOK = () => alert('Verificaremos')
+  const componenteFeedback = (
+    <Feedback 
+      textoOK={textoOK}
+      textoNOK={textoNOK}
+      funcaoOK={funcaoOK}
+      funcaoNOK={funcaoNOK}
+    />
+  )
+  return <div className="container border mt-2">
     <div className="row">
       <div className="col-12">
         {/* i.fa.fa-hippo */}
@@ -9,64 +24,45 @@ const App = () => (
 
     <div className="row">
       <div className="col-sm-12 col-md-6 col-xxl-3">
-        {/* .card>.card-header+.card-body */}
-        <div className="card">
-          <div className="card-header text-muted">15/04/2025</div>
-          <div className="card-body d-flex">
-            <div className="d-flex align-items-center">
-              <i className="fa-solid fa-basketball fa-4x"></i>
-            </div>
-            <div className="ms-2 flex-grow-1 border rounded p-2">
-              <h4 className="text-center">Bola de Basquete</h4>
-              <p className="text-center">Uma bola Mega Swag</p>
-            </div>
-          </div>
-        </div>     
+        <Cartao cabecalho="22/04/2026">
+          <Pedido
+            data="15/04/2025"
+            icone="basketball fa-4x"
+            titulo="Bola de Basquete"
+            descricao="Uma bola Mega Swag" />
+          {componenteFeedback}
+        </Cartao>     
       </div>
       <div className="col-sm-12 col-md-6 col-xxl-3">
-        <div className="card">
-          <div className="card-header text-muted">21/03/2025</div>
-          <div className="card-body d-flex">
-            <div className="d-flex align-items-center">
-              <i class="fa-solid fa-gamepad fa-4x"></i>
-            </div>
-            <div className="ms-2 flex-grow-1 border rounded p-2">
-              <h4 className="text-center">Controle de Videogame</h4>
-              <p className="text-center">Controle Giga Style</p>
-            </div>
-          </div>
-        </div>
+        <Cartao cabecalho="21/03/2025">
+          <Pedido
+            data="15/04/2025"
+            icone="gamepad fa-4x"
+            titulo="Controle de Videogame"
+            descricao="Controle Giga Style" />
+          {componenteFeedback}
+        </Cartao>   
       </div>
       <div className="col-sm-12 col-md-6 col-xxl-3">
-        <div className="card">
-          <div className="card-header text-muted">21/03/2025</div>
-          <div className="card-body d-flex">
-            <div className="d-flex align-items-center">
-              <i class="fa-solid fa-mobile fa-4x"></i>
-            </div>
-            <div className="ms-2 flex-grow-1 border rounded p-2">
-              <h4 className="text-center">Celular Swagsung</h4>
-              <p className="text-center">Celular de última geração</p>
-            </div>
-          </div>
-        </div>
-      </div>
+        <Cartao cabecalho="20/03/2025">
+          <Pedido
+            icone="mobile fa-4x"
+            titulo= "Celular Swagsung"
+            descricao= "Celular de última geração" />
+            {componenteFeedback}
+        </Cartao>
+      </div>        
       <div className="col-sm-12 col-md-6 col-xxl-3">
-        <div className="card">
-          <div className="card-header text-muted">21/03/2025</div>
-          <div className="card-body d-flex">
-            <div className="d-flex align-items-center">
-              <i class="fa-solid fa-glasses fa-4x"></i>
-            </div>
-            <div className="ms-2 flex-grow-1 border rounded p-2">
-              <h4 className="text-center">Óculos</h4>
-              <p className="text-center">Óculos do Belissário</p>
-            </div>
-          </div>
-        </div>
+        <Cartao cabecalho="21/03/2025">
+          <Pedido
+            icone="glasses fa-4x"
+            titulo= "Óculos"
+            descricao= "Óculos do Belissário" />
+            {componenteFeedback}
+        </Cartao>
       </div>
     </div>
   </div>
-)
+}
 
 export default App
